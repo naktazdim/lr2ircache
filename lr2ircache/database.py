02e6dec674313fcef556b7c5209a8f02bf39ff53
nakt <nakt_azdim@walkure.net>
2020-03-29 21:04:03 +0900

    [add] データベースのURLを環境変数で指定できるようにした

--- lr2ircache/database.py
+++ lr2ircache/database.py
@@ -1,12 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = os.environ.get("LR2IRCACHE_DB_URL", "sqlite:///./test.db")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL[:6] == "sqlite" else {}
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
